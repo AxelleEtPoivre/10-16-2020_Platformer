e400b1d117ce59e8da458336f885304fbe3fc28e
Axelle <a.gain-le-marchand@etudiant.ecolemelies.fr>
2020-10-16 17:02:49 +0200

    MovedPlayer.cs Script Created
    InputSystem Added

---
+++ Assets/Scripts/movedplayer.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class movedplayer : MonoBehaviour
+{
+
+    private Inputs inputs;
+
+    private void OnEnable() 
+    {
+        inputs = new Inputs();
+        inputs.Enable();
+        inputs.Player.Move.performed += OnMovedperformed;
+    }
+
+
+    private void OnMovedperformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Move fonctionne");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
